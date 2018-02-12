7e38f854c50e0c81126884e20d0f6bed5f1857d7
Pierre de la Martinière <pierre.de.la.martiniere@gmail.com>
2018-02-12 10:21:31 +0100

    Use SAO as an function instead of an object

--- sao.js
+++ sao.js
@@ -1,28 +1,30 @@
-module.exports = {
-  prompts: {
-    client: {
-      message: 'What is the client of this project?',
-      default: ':ginetta:'
-    },
-    project: {
-      message: 'What is the project name',
-      default: ':website:'
-    },
-    hostLive: {
-      type: 'confirm',
-      message: 'Do you want to host it\'s live/production instance?',
-      default: false
-    },
-    liveURL: {
-      type: 'input',
-      message: 'Do you want to host it\'s live/production instance?',
-      default: ':http://project.client.net/:',
-      when({hostLive}) {
-        return hostLive
+module.exports = () => {
+  return {
+    prompts: {
+      client: {
+        message: 'What is the client of this project?',
+        default: ':ginetta:'
+      },
+      project: {
+        message: 'What is the project name',
+        default: ':website:'
+      },
+      hostLive: {
+        type: 'confirm',
+        message: 'Do you want to host it\'s live/production instance?',
+        default: false
+      },
+      liveURL: {
+        type: 'input',
+        message: 'Do you want to host it\'s live/production instance?',
+        default: ':http://project.client.net/:',
+        when({ hostLive }) {
+          return hostLive
+        }
       }
-    }
-  },
-  showTip: true,
-  gitInit: false,
-  installDependencies: false,
+    },
+    showTip: true,
+    gitInit: false,
+    installDependencies: false,
+  };
 }
